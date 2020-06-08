0cec64dc6ae4d82cd6e5800b0d7031fa842f74e1
sjchoi86 <sungjoon.s.choi@gmail.com>
2020-06-08 10:59:35 -0700

    util added.

--- code/util.py
+++ code/util.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 from scipy.special import erf 
 from scipy.spatial import distance
+import scipy.optimize
 
 t_start_tictoc = time.time()
 def tic():
@@ -103,6 +104,16 @@ def x_sampler(n_sample,x_minmax):
         x_samples.append(x_sample)
     return x_samples # list 
 
+def x_sampler_around_val(n_sample,x_val,x_minmax,scale=0.05):
+    """
+    Sample x as a list from the input domain 
+    """
+    x_samples = []
+    for _ in range(n_sample):
+        x_sample = x_val + scale*np.random.randn(*x_val.shape)*sqrt_safe(x_minmax[:,1]-x_minmax[:,0].reshape((1,-1)))
+        x_samples.append(x_sample)
+    return x_samples # list 
+
 def plot_line(
     x,y,fmt='-',lc='k',lw=2,label=None,
     x2=None,y2=None,fmt2='-',lc2='k',lw2=2,ms2=12,mfc2='none',mew2=2,label2=None,
@@ -110,6 +121,7 @@ def plot_line(
     x4=None,y4=None,fmt4='-',lc4='k',lw4=2,ms4=12,mfc4='none',mew4=2,label4=None,
     x5=None,y5=None,fmt5='-',lc5='k',lw5=2,ms5=12,mfc5='none',mew5=2,label5=None,
     x6=None,y6=None,fmt6='-',lc6='k',lw6=2,ms6=12,mfc6='none',mew6=2,label6=None,
+    x7=None,y7=None,fmt7='-',lc7='k',lw7=2,ms7=12,mfc7='none',mew7=2,label7=None,
     x_fb=None,y_fb_low=None,y_fb_high=None,fba=0.1,fbc='g',labelfb=None,
     figsize=(10,5),
     xstr='',xfs=12,ystr='',yfs=12,
@@ -131,7 +143,8 @@ def plot_line(
         plt.plot(x5,y5,fmt5,color=lc5,linewidth=lw5,ms=ms5,mfc=mfc5,mew=mew5,label=label5)
     if (x6 is not None):
         plt.plot(x6,y6,fmt6,color=lc6,linewidth=lw6,ms=ms6,mfc=mfc6,mew=mew6,label=label6)
-
+    if (x7 is not None):
+        plt.plot(x7,y7,fmt7,color=lc7,linewidth=lw7,ms=ms7,mfc=mfc7,mew=mew7,label=label7)
 
     if (x_fb is not None):
         plt.fill_between(x_fb.reshape(-1),
@@ -169,11 +182,16 @@ def k_m52(x1,x2,x_range=1.0,gain=1.0,invlen=5.0):
     K = gain*(1+sqrt_safe(5*R_sq)+(5.0/3.0)*R_sq)*np.exp(-sqrt_safe(5*R_sq))
     return K
 
-def gp_m52(x,y,x_test,gain=1.0,invlen=5.0,eps=1e-8):
+def gp_m52(x,y,x_test,x_minmax=None,gain=1.0,invlen=5.0,eps=1e-8):
     """
     Gaussian process with ARD Matern 5/2 Kernel
     """
-    x_range = np.max(x,axis=0)-np.min(x,axis=0)
+    if x_minmax is None:
+        x_range = np.max(x,axis=0)-np.min(x,axis=0)
+    else:
+        xmin,xmax = x_minmax[:,0],x_minmax[:,1]
+        x_range = xmax-xmin
+    
     k_test = k_m52(x_test,x,x_range=x_range,gain=gain,invlen=invlen)
     K = k_m52(x,x,x_range=x_range,gain=gain,invlen=invlen)
     n = x.shape[0]
@@ -189,7 +207,7 @@ def Phi(x):
     """
     return (1.0 + erf(x / math.sqrt(2.0))) / 2.0
 
-def acquisition_function(x_bo,y_bo,x_test,SCALE_Y=True,gain=1.0,invlen=5.0,eps=1e-6):
+def acquisition_function(x_bo,y_bo,x_test,x_minmax=None,SCALE_Y=True,gain=1.0,invlen=5.0,eps=1e-6):
     """
     Acquisition function of Bayesian Optimization with Expected Improvement
     """
@@ -203,8 +221,7 @@ def acquisition_function(x_bo,y_bo,x_test,SCALE_Y=True,gain=1.0,invlen=5.0,eps=1
     else:
         y_bo_scaled = np.copy(y_bo)
     
-    mu_test,var_test = gp_m52(x_bo,y_bo_scaled,x_test,gain=gain,invlen=invlen,eps=eps)
-    eps = 1e-6
+    mu_test,var_test = gp_m52(x_bo,y_bo_scaled,x_test,x_minmax=x_minmax,gain=gain,invlen=invlen,eps=eps)
     gamma = (np.min(y_bo_scaled) - mu_test)/sqrt_safe(var_test)
     a_ei = 2.0 * sqrt_safe(var_test) * (gamma*Phi(gamma) + norm.pdf(mu_test,0,1))
     
@@ -212,6 +229,83 @@ def acquisition_function(x_bo,y_bo,x_test,SCALE_Y=True,gain=1.0,invlen=5.0,eps=1
         mu_test = 0.5 * y_range * mu_test + y_bo_mean
     
     return a_ei,mu_test,var_test
+
+def argmax_acquisition(x_data,y_data,x_minmax,
+                       n_retry=10,kgain=1.0,invlen=10,eps=1e-6,maxiter=100,
+                       n_cd_step=0,n_cd_resolution=0):
+    """
+    Get the maxima of the acquisition function from multiple random restarts 
+    """
+    xdim = x_minmax.shape[0]
+    # First, compuate GP statistics
+    xmin,xmax = x_minmax[:,0],x_minmax[:,1]
+    x_range = xmax-xmin
+    K = k_m52(x1=x_data,x2=x_data,x_range=x_range,gain=kgain,invlen=invlen) # kernel matrix
+    inv_K = np.linalg.inv(K+eps*np.eye(x_data.shape[0]))
+    def a_test(x_test,SCALE_Y=True):
+        """
+        Acquisition function with precomputed invK
+        """
+        if SCALE_Y:
+            y_data_copy = np.copy(y_data)
+            y_data_mean = np.mean(y_data_copy)
+            y_data_mz = y_data_copy - y_data_mean
+            y_min,y_max = np.min(y_data_mz), np.max(y_data_mz)
+            y_range = y_max - y_min
+            y_data_scaled = 2.0 * y_data_mz / y_range
+        else:
+            y_data_scaled = np.copy(y_data)
+        # Get GP mu and var
+        k_test = k_m52(x1=x_test,x2=x_data,
+                       x_range=x_range,gain=kgain,invlen=invlen)
+        W = np.matmul(k_test,inv_K)
+        mu_y = np.mean(y_data_scaled)
+        mu_test = np.matmul(W,y_data_scaled-mu_y)+mu_y
+        var_test = (kgain-np.diag(np.matmul(W,k_test.T))).reshape((-1,1))
+        # Expected improvement criterion 
+        gamma = (np.min(y_data_scaled) - mu_test)/sqrt_safe(var_test)
+        a_ei = 2.0 * sqrt_safe(var_test) * (gamma*Phi(gamma) + norm.pdf(mu_test,0,1))
+        if SCALE_Y:
+            mu_test = 0.5 * y_range * mu_test + y_data_mean
+        return a_ei,mu_test,var_test
+    def f_temp(x):
+        """
+        Cost function to be minimized = -acquisition function
+        """
+        a_ei,mu_test,var_test = a_test(x.reshape((1,-1)))
+        return -a_ei # flip as we will find minima
+    # Unifromly find initial inputs using k-DPP
+    x_inits = get_x_sub_kdpp(x_minmax,n_sel=n_retry,n_raw=5000,invlen=100)
+    # Skip half of them to start near 'x_best'
+    x_bset,_ = get_best_xy(x_data,y_data)
+    x_inits[n_retry//2:,:] = \
+        x_bset + 0.1*sqrt_safe(x_minmax[:,1]-x_minmax[:,0]).reshape((1,-1))*np.random.randn(n_retry//2,xdim)
+    
+    # Get the maxima of the acquisition function from multiple initial points
+    x_opts,f_opts,f_inits = np.zeros((n_retry,xdim)),np.zeros(n_retry),np.zeros(n_retry)
+    for i_idx,x_init in enumerate(x_inits): # multiple retry 
+        f_inits[i_idx] = f_temp(x_init)
+        x_opt = scipy.optimize.fmin(func=f_temp,x0=x_init,disp=0,maxiter=maxiter) # optimized input [1 x d]
+        x_opt = np.maximum(np.minimum(x_opt,xmax),xmin) # clip values with x_minmax
+        x_opt = x_opt.reshape((1,-1)) # rank 2 
+        f_opt = f_temp(x_opt)
+        x_opts[i_idx,:],f_opts[i_idx] = x_opt,f_opt    
+    x_best = x_opts[np.argmin(f_opts),:]
+    # Fine-tuning with coordinate descent
+    if n_cd_step > 0:
+        for _ in range(n_cd_step):
+            for d_idx in range(xdim): # for different dim
+                x_best_copy = np.copy(x_best) # copy
+                xmin_d,xmax_d = xmin[d_idx],xmax[d_idx] # range 
+                f_vals,x_d_vals = np.zeros(n_cd_resolution),np.linspace(xmin_d,xmax_d,n_cd_resolution)
+                for i_idx,x_d in enumerate(x_d_vals):
+                    if i_idx == 0: # reserve the first one to be the current solution 
+                        x_best_copy[d_idx] = x_best[d_idx]  
+                    else:
+                        x_best_copy[d_idx] = x_d 
+                    f_vals[i_idx] = f_temp(x_best_copy) # find minima
+                x_best[d_idx] = x_d_vals[np.argmin(f_vals)] # update
+    return x_best.reshape((1,-1)) 
 
 def scale_to_match_range(x_to_change,y_to_refer):
     """
@@ -248,7 +342,7 @@ def sample_from_best_voronoi_cell(x_data,y_data,x_minmax,n_sample,
             else:
                 # Gaussian sampling centered at x_sel
                 eps = 1e-6
-                x_sel = x_sol + 0.1*np.random.randn(*x_sol.shape)*sqrt_safe(x_minmax[:,1]-x_minmax[:,0].reshape((1,-1)))
+                x_sel = x_sol + 0.05*np.random.randn(*x_sol.shape)*sqrt_safe(x_minmax[:,1]-x_minmax[:,0].reshape((1,-1)))
                 
             dist_sel = r_sq(x_data,x_sel)
             idx_min_sel = np.argmin(dist_sel)
@@ -296,7 +390,9 @@ def get_x_sub_kdpp(x_minmax,n_sel,n_raw=10000,invlen=100):
 
 def run_bavoo(
     func_eval,x_minmax,USE_RAY=True,
-    n_random=1,n_bo=1,n_voo=1,n_cd=1,USE_KDPP=False,
+    n_random=1,n_bo=1,USE_CONSTANT_LIER=False,USE_BEST_CENTER_HEURISTICS=False,
+    n_voo=1,n_sample_for_voo=1,
+    n_cd=1,
     n_data_max=100,n_worker=10,seed=0,
     n_sample_for_bo=2000,gain=1.0,invlen=5.0,eps=1e-6,
     max_try_sbv=5000,
@@ -339,7 +435,7 @@ def run_bavoo(
         # Random sample
         iclk_random = time.time()
         for _ in range(n_random):
-            x_evals = x_sampler(n_sample=n_worker,x_minmax=x_minmax)
+            x_evals = x_sampler(n_sample=n_worker,x_minmax=x_minmax)    
             if USE_RAY:
                 evals = [func_eval_ray.remote(x=x_eval) for x_eval in x_evals] # EVALUATE
                 y_evals = ray.get(evals)
@@ -362,36 +458,68 @@ def run_bavoo(
         # Bayesian Optimization
         iclk_bo = time.time()
         for _ in range(n_bo):
-            # Constant liar model for parallelizing BO
-            x_evals,x_data_copy,y_data_copy = [],np.copy(x_data),np.copy(y_data)
-            for _ in range(n_worker):
-                if USE_KDPP:
-                    x_checks = get_x_sub_kdpp(x_minmax,n_sample_for_bo,n_raw=n_sample_for_bo*2)
+            if USE_CONSTANT_LIER:
+                # Constant liar model for parallelizing BO
+                x_evals,x_data_copy,y_data_copy = [],np.copy(x_data),np.copy(y_data)
+                for _ in range(n_worker):    
+                    if (np.random.rand() < 0.5) or (USE_BEST_CENTER_HEURISTICS is False):
+                        x_checks = np.asarray(x_sampler(n_sample_for_bo,x_minmax=x_minmax))[:,0,:]    
+                    else:
+                        # Gaussian sampling centered at x_sel
+                        x_sol,_ = get_best_xy(x_data,y_data)
+                        x_checks = np.asarray(x_sampler_around_val(
+                            n_sample_for_bo,x_val=x_sol,x_minmax=x_minmax,scale=0.05))[:,0,:]    
+                    eps = 1e-6
+                    a_ei,mu_checks,_ = acquisition_function(
+                        x_data_copy,y_data_copy,x_checks,x_minmax=x_minmax,
+                        gain=gain,invlen=invlen,eps=eps) # get the acquisition values 
+                    max_idx = np.argmax(a_ei) # select the one with the highested value 
+                    # As we cannot get the actual y_eval from the real evaluation, we use the constant liar model
+                    # that uses the GP mean to approximate the actual evaluation value. 
+                    x_liar,y_liar = x_checks[max_idx,:].reshape((1,-1)),mu_checks[max_idx].reshape((1,-1))
+                    # Append
+                    x_data_copy = np.concatenate((x_data_copy,x_liar),axis=0)
+                    y_data_copy = np.concatenate((y_data_copy,y_liar),axis=0)
+                    x_evals.append(x_liar) # append the inputs to evaluate 
+                # Evaluate k candidates in one scoop 
+                if USE_RAY:
+                    evals = [func_eval_ray.remote(x=x_eval) for x_eval in x_evals] # EVALUATE
+                    y_evals = ray.get(evals)
                 else:
-                    x_checks = np.asarray(x_sampler(n_sample_for_bo,x_minmax=x_minmax))[:,0,:]    
-                
-                a_ei,mu_checks,_ = acquisition_function(
-                    x_data_copy,y_data_copy,x_checks,gain=gain,invlen=invlen,eps=eps) # get the acquisition values 
-                max_idx = np.argmax(a_ei) # select the one with the highested value 
-                # As we cannot get the actual y_eval from the real evaluation, we use the constant liar model
-                # that uses the GP mean to approximate the actual evaluation value. 
-                x_liar,y_liar = x_checks[max_idx,:].reshape((1,-1)),mu_checks[max_idx].reshape((1,-1))
-                # Append
-                x_data_copy = np.concatenate((x_data_copy,x_liar),axis=0)
-                y_data_copy = np.concatenate((y_data_copy,y_liar),axis=0)
-                x_evals.append(x_liar) # append the inputs to evaluate 
-                
-            # Evaluate k candidates in one scoop 
-            if USE_RAY:
-                evals = [func_eval_ray.remote(x=x_eval) for x_eval in x_evals] # EVALUATE
-                y_evals = ray.get(evals)
+                    y_evals = [func_eval(x=x_eval) for x_eval in x_evals] 
+                x_bo = np.asarray(x_evals)[:,0,:]
+                y_bo = np.asarray(y_evals)[:,0,:]
+                # Concatenate BO results
+                x_data = np.concatenate((x_data,x_bo),axis=0)
+                y_data = np.concatenate((y_data,y_bo),axis=0)
             else:
-                y_evals = [func_eval(x=x_eval) for x_eval in x_evals] 
-            x_bo = np.asarray(x_evals)[:,0,:]
-            y_bo = np.asarray(y_evals)[:,0,:]
-            # Concatenate BO results
-            x_data = np.concatenate((x_data,x_bo),axis=0)
-            y_data = np.concatenate((y_data,y_bo),axis=0)
+                # Bayesian Optimization without Parralel Processeing 
+                """ # Sampling-based argmax, not working well on high-dim
+                # First, select which input to evaluate based on acquisition function 
+                if (np.random.rand() < 0.5) or (USE_BEST_CENTER_HEURISTICS is False):
+                    x_checks = np.asarray(x_sampler(n_sample_for_bo,x_minmax=x_minmax))[:,0,:]    
+                else:
+                    # Gaussian sampling centered at x_sel
+                    x_sol,_ = get_best_xy(x_data,y_data)
+                    x_checks = np.asarray(x_sampler_around_val(
+                        n_sample_for_bo,x_val=x_sol,x_minmax=x_minmax,scale=0.05))[:,0,:]    
+                eps = 1e-6
+                a_ei,mu_checks,_ = acquisition_function(
+                    x_data,y_data,x_checks,x_minmax=x_minmax,
+                    gain=gain,invlen=invlen,eps=eps) # get the acquisition values 
+                max_idx = np.argmax(a_ei) # select the one with the highested value 
+                x_eval = x_checks[max_idx,:].reshape((1,-1)) # input to evaluate [1, xdim]
+                """
+                x_eval = argmax_acquisition(x_data,y_data,x_minmax,
+                                            n_retry=4,kgain=1.0,invlen=10,eps=eps,
+                                            maxiter=100,
+                                            n_cd_step=0,n_cd_resolution=100)
+                
+                # Do evaluate 
+                y_eval = func_eval(x_eval)
+                # Concatenate BO results
+                x_data = np.concatenate((x_data,x_eval),axis=0)
+                y_data = np.concatenate((y_data,y_eval),axis=0)
         esec_bo = time.time() - iclk_bo
         esec_total = time.time() - iclk_total
         # Plot BO
@@ -408,7 +536,7 @@ def run_bavoo(
         for _ in range(n_voo):
             # Get input points to eval from sampling the best Voronoi cell 
             x_evals = sample_from_best_voronoi_cell(
-                x_data,y_data,x_minmax,n_sample=n_worker,max_try_sbv=max_try_sbv)
+                x_data,y_data,x_minmax,n_sample=n_sample_for_voo,max_try_sbv=max_try_sbv)
             # Evaluate
             if USE_RAY:
                 evals = [func_eval_ray.remote(x=x_eval) for x_eval in x_evals] # EVALUATE
@@ -431,9 +559,7 @@ def run_bavoo(
         iclk_cd = time.time()
         for _ in range(n_cd):
             x_sol,y_sol = get_best_xy(x_data,y_data)
-            # for d_idx in range(x_dim): # for each dim
-            if True:
-                d_idx = np.random.permutation(x_dim)[0] # select one coordinate at a time 
+            for d_idx in np.random.permutation(x_dim): # for each dim with permuation 
                 x_minmax_d = x_minmax[d_idx,:]
                 x_sample_d = x_minmax_d[0]+(x_minmax_d[1]-x_minmax_d[0])*np.random.rand(n_worker)
                 x_sample_d[0] = x_sol[0,d_idx]
